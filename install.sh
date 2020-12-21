ec1d3ef61321b37e4874d738b23dc2b17588b9f5
michael080808 <1101115223@qq.com>
2020-12-21 20:59:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,16 +15,16 @@ ogre_version=v1.12.10
 opencv_version=4.5.0
 
 # Update Sources
-sudo -E apt update
+sudo apt update
 
 # Install Base Tools
-sudo -E apt install build-essential net-tools git cmake cmake-qt-gui
+sudo apt install build-essential net-tools git cmake cmake-qt-gui
 
 # Install Java Development Kits
-sudo -E apt install openjdk-11-jdk
+sudo apt install openjdk-11-jdk
 
 # Install Python Development Environments
-sudo -E apt install python3.8-dev python3.8-doc python3.8-dbg python3-distutils-extra
+sudo apt install python3.8-dev python3.8-doc python3.8-dbg python3-distutils-extra
 
 # Intel MKL - Optional
 if [[ $(cat /proc/cpuinfo | grep "model name")=~"Intel" ]]
@@ -62,48 +62,50 @@ then
 fi
 
 # Python PIP
-sudo -E apt install -y python3-pip
+sudo apt install -y python3-pip
 # Update PIP
 sudo -E -H pip3 install -U pip setuptools wheel
 
 # Python Numpy
-sudo -E apt install -y python3-numpy
+sudo apt install -y python3-numpy
 # Update Numpy
-sudo -E -H pip3 install -U numpy
+sudo -H pip3 install -U numpy -i https://pypi.tuna.tsinghua.edu.cn/simple
 
 # Tensorflow
-sudo -E -H pip3 install -U tensorflow
-# PyTorch
-sudo -E -H pip3 install -U torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio===0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+sudo -H pip3 install -U tensorflow -i https://pypi.tuna.tsinghua.edu.cn/simple
+# PyTorch - Part 1
+sudo -E -H pip3 install -U torch==1.7.1+cu110 torchvision==0.8.2+cu110 -f https://download.pytorch.org/whl/torch_stable.html
+# PyTorch - Part 2
+sudo -H pip3 install -U torchaudio===0.7.2 -i https://pypi.tuna.tsinghua.edu.cn/simple 
 
 # Qt Creator
-sudo -E apt install -y qtcreator
+sudo apt install -y qtcreator
 
 # OpenCV Dependencies - CCache
-sudo -E apt install -y ccache
+sudo apt install -y ccache
 
 # OpenCV Dependencies - BLAS
-sudo -E apt install -y libblas-dev libblas64-dev
+sudo apt install -y libblas-dev libblas64-dev
 # OpenCV Dependencies - LAPACK
-sudo -E apt install -y liblapack-dev liblapack64-dev
+sudo apt install -y liblapack-dev liblapack64-dev
 # OpenCV Dependencies - LAPACKE
-sudo -E apt install -y liblapacke-dev liblapacke64-dev
+sudo apt install -y liblapacke-dev liblapacke64-dev
 # OpenCV Dependencies - ATLAS
-sudo -E apt install -y libatlas-base-dev libatlas-cpp-0.6-dev
+sudo apt install -y libatlas-base-dev libatlas-cpp-0.6-dev
 # OpenCV Dependencies - OpenBLAS
-sudo -E apt install -y libopenblas-dev libopenblas64-dev
+sudo apt install -y libopenblas-dev libopenblas64-dev
 
 # OpenCV Dependencies - GTK & VTK
-sudo -E apt install -y libgtk-3-dev libvtk7-dev
+sudo apt install -y libgtk-3-dev libvtk7-dev
 # OpenCV Dependencies - Images
-sudo -E apt install -y zlib1g-dev libjpeg-turbo8-dev libwebp-dev libpng-dev libtiff-dev libopenjp2-7-dev libopenexr-dev
+sudo apt install -y zlib1g-dev libjpeg-turbo8-dev libwebp-dev libpng-dev libtiff-dev libopenjp2-7-dev libopenexr-dev
 # OpenCV Dependencies - Videos
-sudo -E apt install -y libdc1394-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libavresample-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt install -y libdc1394-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libavresample-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 
 # OpenCV Dependencies - Java Supports
-sudo -E apt install -y ant
+sudo apt install -y ant
 # Ant - Fix Link Bug
-sudo -E rm -rf /usr/bin/ant && sudo -E ln -s /usr/share/ant/bin/ant /usr/bin/ant
+sudo rm -rf /usr/bin/ant && sudo ln -s /usr/share/ant/bin/ant /usr/bin/ant
 
 # OpenCV Dependencies - Google Glog & Gflags
 sudo -E apt install -y libgflags-dev libgoogle-glog-dev
@@ -113,7 +115,7 @@ sudo -E apt install -y libboost-all-dev libtesseract-dev
 # OpenCV Dependencies - OGRE
 git clone https://github.com/ogrecave/ogre
 # OGRE Dependencies
-sudo -E apt install -y libgles2-mesa-dev libxt-dev libxaw7-dev libsdl2-dev libfreetype-dev libfreeimage-dev nvidia-cg-toolkit
+sudo apt install -y libgles2-mesa-dev libxt-dev libxaw7-dev libsdl2-dev libfreetype-dev libfreeimage-dev nvidia-cg-toolkit
 # OGRE Build
 cd ./ogre
 git checkout $ogre_version
